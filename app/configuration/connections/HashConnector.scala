fc47cc71b1b775b282a8f1e16f8ae37719d6555c
boniface <boniface@kabaso.com>
2018-12-13 12:46:02 +0200

    Updated Cluster Details

--- app/configuration/connections/HashConnector.scala
+++ app/configuration/connections/HashConnector.scala
@@ -16,14 +16,15 @@ object Configuration {
 
   def PORT = 9042
 
-  def connectionTimeoutMillis = 700000
+  def connectionTimeoutMillis = 70000000
 
   // Default is 5000
-  def readTimeoutMillis = 1500000
+  def readTimeoutMillis = 150000000
 
   // Default is 12000
   val hosts: Seq[String] = config.getStringList("cassandra.host").asScala.toList
-  val dataCenter: String = config.getString("cassandra.dataCenter")
+  val dataCenter1: String = config.getString("cassandra.dataCenter1")
+  val dataCenter2: String = config.getString("cassandra.dataCenter2")
   val inets: Seq[InetAddress] = hosts.map(InetAddress.getByName)
   val keySpace: String = config.getString("cassandra.keySpace")
   val clusterName: String = config.getString("cassandra.clusterName")
@@ -34,13 +35,16 @@ object Configuration {
 object DataConnection {
 
   val devKeySpaceQuery = KeySpace(Configuration.keySpace).ifNotExists()
-    .`with`(replication eqs SimpleStrategy.replication_factor(1))
-    .and(durable_writes eqs true)
-
-  val prodKeySpaceQuery =  KeySpace(Configuration.keySpace).ifNotExists()
     .`with`(replication eqs NetworkTopologyStrategy
-      .data_center("data1", 2)
-      .data_center("data2", 3)
+      .data_center(Configuration.dataCenter1, 3)
+      .data_center(Configuration.dataCenter2, 2)
+    ).and(durable_writes eqs true)
+
+
+  val requestQuery = KeySpace(Configuration.keySpace).ifNotExists()
+    .`with`(replication eqs NetworkTopologyStrategy
+      .data_center(Configuration.dataCenter1, 3)
+      .data_center(Configuration.dataCenter2, 2)
     ).and(durable_writes eqs true)
 
   lazy val connector = ContactPoints(Configuration.hosts, Configuration.PORT)
@@ -52,11 +56,11 @@ object DataConnection {
         .withPoolingOptions(new PoolingOptions()
           .setMaxQueueSize(100000)
           .setPoolTimeoutMillis(20000))
-        .withLoadBalancingPolicy(
-          new DCAwareRoundRobinPolicy.Builder()
-            .withUsedHostsPerRemoteDc(1)
-            .withLocalDc(Configuration.dataCenter).build()
-        )
-    ).noHeartbeat().keySpace(devKeySpaceQuery)
-
+              .withLoadBalancingPolicy(
+                new DCAwareRoundRobinPolicy.Builder()
+                  .withUsedHostsPerRemoteDc(1)
+                  .withLocalDc(Configuration.dataCenter2).build()
+              )
+    )
+    .noHeartbeat().keySpace(requestQuery)
 }
